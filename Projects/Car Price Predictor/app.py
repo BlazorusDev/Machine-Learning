cf5156400a8fd553a1eaa5901360bcb3ff6df77d
Mohit-Mehra <info.mohit728@gmail.com>
2024-07-18 21:42:40 +0530

    more

--- Projects/Car Price Predictor/app.py
+++ Projects/Car Price Predictor/app.py
@@ -1,11 +1,26 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, redirect
+import pandas as pd
+import numpy as np
+
 
 app = Flask(__name__)
+
+car = pd.read_csv("d:\ML\Projects\Car Price Predictor\Cleaned_Car.csv")
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    companies = sorted(car["company"].unique())
+    car_models = sorted(car["name"].unique())
+    year = sorted(car["year"].unique(), reverse=True)
+    fuel_type = car["fuel_type"].unique()
+    return render_template(
+        "index.html",
+        companies=companies,
+        car_models=car_models,
+        years=year,
+        fuel_type=fuel_type,
+    )
 
 
 if __name__ == "__main__":
